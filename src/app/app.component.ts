f1ba19773054b3fb3b0759bd881c4233e1a1a3ac
zarkonow <stojanov1987@gmail.com>
2025-06-25 14:25:49 +0200

    feat: restructure Angular application with new module and component setup
    - Updated app.component.ts to include server management logic and status handling.
    - Refactored app.component.spec.ts to align with new component structure and updated title.
    - Created app.module.ts to define the main application module and imported necessary modules.
    - Changed index.html title from 'PipesAssignmentStart' to 'MyFirstApp'.
    - Modified main.ts to bootstrap the application using AppModule and enable production mode.
    - Added environment files for production and development configurations.
    - Included polyfills and test configuration files for better compatibility and testing setup.
    - Established a browserslist file for specifying supported browsers.
    - Added Karma configuration for testing framework setup.
    - Updated tslint.json for custom linting rules related to Angular component selectors.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes-assignment-start';
+  appStatus = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('stable');
+    }, 2000);
+  });
+  servers = [
+    {
+      instanceType: 'medium',
+      name: 'Production',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'large',
+      name: 'User Database',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Development Server',
+      status: 'offline',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Testing Environment Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    }
+  ];
+  filteredStatus = '';
+  getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
+    return {
+      'list-group-item-success': server.status === 'stable',
+      'list-group-item-warning': server.status === 'offline',
+      'list-group-item-danger': server.status === 'critical'
+    };
+  }
+  onAddServer() {
+    this.servers.push({
+      instanceType: 'small',
+      name: 'New Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    });
+  }
 }
